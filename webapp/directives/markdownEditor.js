852495c269ee32ed94c42a79fdf60e934a9c7dd5
Patrick Lorio <patrick@lorio.me>
2014-10-21 15:28:26 -0700

    But markdownEditor code into link function

--- webapp/directives/markdownEditor.js
+++ webapp/directives/markdownEditor.js
@@ -1,67 +1,60 @@
 var app = app || angular.module('app');
 
-app.directive('markdownEditor', function () {
+app.directive('markdownEditor', function ($sce) {
     return {
         restrict: 'E',
         replace: true,
-        controller: function ($scope, $sce) {
-            var equations = {};
-
-            function exFn() {
+        link: function (scope) {
+            var equationCache = {};
+            var converter = new Showdown.converter({ extensions: [function () {
                 return [
                     {
                         type: 'lang',
                         regex: '(~D){2}(.*?)(~D){2}',
                         replace: function (match, prefix, content) {
-                            if (content in equations) {
-                                return equations[content];
+                            if (content in equationCache) {
+                                return equationCache[content];
                             }
 
                             var item = $('<span>' + content + '</span>').mathquill();
-                            equations[content] = item.text().length == 2? 'ERROR' : item.prop('outerHTML');
-                            return equations[content];
+                            equationCache[content] = item.text().length == 2 ? 'ERROR' : item.prop('outerHTML');
+                            return equationCache[content];
                         }
                     }
                 ]
-            }
+            }]});
 
-            var converter = new Showdown.converter({ extensions: [exFn] });
-            $scope.lines = [];
-            $scope.cursor = {line: 0, ch: 0};
+            scope.lines = [];
+            scope.cursor = {line: 0, ch: 0};
 
-            $scope.onChange = function (startLine, endLine) {
-                for (var i = startLine; i < Math.min(endLine + 1, $scope.lines.length); ++i) {
-                    prepLine($scope.lines[i - 1], $scope.lines[i], $scope.lines[i + 1], i);
+            scope.onChange = function (startLine, endLine) {
+                for (var i = startLine; i < Math.min(endLine + 1, scope.lines.length); ++i) {
+                    prepLine(scope.lines[i - 1], scope.lines[i], scope.lines[i + 1], i);
                 }
             };
 
             var mId = 0;
-
-            function getStyleLine(line) {
-                if (!line) return '';
-                if (line.text.indexOf('---') == 0 || line.text.indexOf('===') == 0) {
-                    return '\n' + line.text;
-                }
-            }
-
+            var newLine = $sce.trustAsHtml('<br/>');
             function prepLine(prevLine, line, nextLine, index) {
-                line.mId = mId++;
+                function getStyleLine(line) {
+                    if (!line) return '';
+                    if (line.text.indexOf('---') == 0 || line.text.indexOf('===') == 0) {
+                        return '\n' + line.text;
+                    }
+                }
 
+                line.mId = mId++;
                 if (line.text.indexOf('===') == 0 || line.text.indexOf('---') == 0) {
                     prepLine(null, prevLine, line);
                 }
-
                 if (line.text.indexOf('===') == 0) {
                     line.html = $sce.trustAsHtml(' ');
                     return;
                 }
-
                 line.html = $sce.trustAsHtml(converter.makeHtml((line.text || '') + (getStyleLine(nextLine) || '')));
             }
 
-            var newLine = $sce.trustAsHtml('<br/>');
-
-            $scope.lineMod = function (html) {
+            scope.lineMod = function (html) {
                 if (!html) return newLine;
                 return html;
             }
